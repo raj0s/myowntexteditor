8c73dc834493f85acfb3ce88221674b0d48bb485
raj0s <aryamanrajpal04@gmail.com>
2025-07-15 17:09:59 +1000

    Removing out/Debug from final submission hopefully

--- dwerb.c
+++ dwerb.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/errno.h>
+// ioctl is used to grab the size of the terminal window that is currently being
+// worked in
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -10,12 +13,32 @@
 // taking a parameter k and using a BITWISE AND operation to set it to the
 // Ctrl+parameter key
 //
-#define CTRL_KEY(k) ((k) & 0x1f)
 
 /**** data ****/
-struct termios orig_termios;
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+// create a struct which holds the holds the state of the terminal
+struct editorConfig {
+  struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 /**** terminal ****/
+
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    // we pass the values by setting the values to the int references of cols
+    // and rows that were passed orignally in the function this helps C returns
+    // multiple things along with having functions return multiple values in C.
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
 // error handling, gives out what char gave the error
 void die(const char *s) {
   write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -25,18 +48,18 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
     die("tcsetattr");
   }
 }
 
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
     die("tcgetattr");
   }
 
   atexit(disableRawMode);
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   // ICANON helps read input byte by byte instead of line by line
   // ISIG prevents Ctrl+z SIGTSTP and Ctrl+c which gives out SIGINT
   // IXON is to prevent Ctrl+s and Ctrl+q to stop and continue transmission to
